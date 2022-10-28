72d9dcacc2ec248824d924e9e26f2699fb0dd6fa
DAVID-QUILLOT Mathis <id1988@DOMENSAI.ECOLE>
2022-10-28 09:31:37 +0200

    Vues : Les classes AccueilKata, AccueilPerso, CreerCompte et Connexion fonctionnent

---
+++ view/connectionview.py
@@ -0,0 +1,42 @@
+from email.message import Message
+from pprint import pprint
+
+
+from InquirerPy import inquirer
+from InquirerPy.base.control import Choice
+
+from view.abstractview import AbstractView
+from view.session import Session
+
+
+
+
+ASK_PSEUDO=inquirer.text(message = 'Quel est ton pseudo?')
+
+
+
+class ConnectionView(AbstractView):
+
+
+    def display_info(self):
+        print(f"Connexion au compte")
+
+    def make_choice(self):
+        pseudo = ASK_PSEUDO.execute()
+        from client_joueur import ClientJoueur
+        clientjoueur = ClientJoueur()
+        if type(clientjoueur.get_id(pseudo)) == int :
+            #Compléter les infos de la session
+            from view.session import Session
+            session = Session(pseudo)
+            print(session.pseudo)
+            from view.accueilpersoview import AccueilPersoView
+            return AccueilPersoView()
+           
+        else :
+            print("Le pseudo n'existe pas") 
+            from view.accueilkataview import AccueilKataView
+            return AccueilKataView()
+            
+        
+        
---
+++ view/creercompteview.py
@@ -0,0 +1,36 @@
+from email.message import Message
+from pprint import pprint
+
+
+from InquirerPy import inquirer
+from InquirerPy.base.control import Choice
+
+from view.abstractview import AbstractView
+from view.session import Session
+
+
+
+
+ASK_PSEUDO=inquirer.text(message = 'Entre un pseudo')
+
+
+
+class CreerCompteView(AbstractView):
+
+
+    def display_info(self):
+        print(f"Création du compte")
+
+    def make_choice(self):
+        pseudo = ASK_PSEUDO.execute()
+        from client_joueur import ClientJoueur
+        clientjoueur = ClientJoueur()
+        if clientjoueur.get_id(pseudo) == None :
+            #Le joueur est inséré dans la base de données 
+            clientjoueur.create_joueur(pseudo = pseudo)
+        else :
+            #Message d'erreur
+            print("Le pseudo existe déjà")
+        #Dans tous les cas, on revient ensuite à l'écran d'accueil
+        from view.accueilkataview import AccueilKataView
+        return AccueilKataView()
