110aae63229cf8c14c6ca91ce966e271db2d9e63
Mathieu Thomassin <mathieu.thomassin@eleve.ensai.fr>
2022-11-20 17:19:30 +0100

    pylint abstract_generer_mot.py

--- business_objects/abstract_generer_mot.py
+++ business_objects/abstract_generer_mot.py
@@ -1,9 +1,21 @@
-from abc import *
+"""permet d'avoir une classe mère abstraite pour générer des mots
+"""
+from abc import ABC, abstractmethod
 
 class AbstractGenererMot(ABC):
+    # pylint: disable=too-few-public-methods
+    """classe abstraite mère pour la génération de mot
+
+    Parameters
+    ----------
+    ABC : ABC
+        la classe de base du package abc
+    """
     def __init__(self):
-        pass
+        """constructeur abstrait vide
+        """
 
     @abstractmethod
     def generer(self):
-        pass
+        """méthode abstraite vide
+        """
