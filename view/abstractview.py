7779f8f442bdb1b37f0816999caf6773747590d0
Mathieu Thomassin <mathieu.thomassin@eleve.ensai.fr>
2022-11-21 12:28:54 +0100

    pylint abstractview

--- view/abstractview.py
+++ view/abstractview.py
@@ -1,14 +1,20 @@
+"""classe abstraite pour les vues
+"""
+
 from abc import ABC, abstractmethod
 
-from view.session import Session
-
-
 class AbstractView(ABC):
-
+    """classe abstraite pour les vues en console
+    """
+    #pylint: disable=unnecessary-pass
     @abstractmethod
     def display_info(self):
+        """méthode abstraite pour afficher uniquement de l'information en console
+        """
         pass
 
     @abstractmethod
     def make_choice(self):
+        """méthode abstraite pour afficher un choix à faire par l'utilisateur en console
+        """
         pass
