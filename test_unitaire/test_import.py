3d86ad98d2fbc26845a05276a3d707f712649922
Mathieu Thomassin <mathieu.thomassin@eleve.ensai.fr>
2022-11-21 12:15:58 +0100

    changement dans la place de fichiers de test importation

--- test_unitaire/test_import.py
+++ test_unitaire/test_import.py
@@ -1,9 +1,10 @@
+import os
 import csv
 import sys
 from importation_objects.abstract_importation_liste import AbstractImportationListe
 
 
-fichier = "listeformatCSV.csv" #le nom du fichier de test
+FICHIER = "listeformatCSV.csv" #le nom du fichier de test
 dossier = sys.path[1] #récupération chemin du projet
 dossier = dossier.replace(os.sep, '/') #transformation
 
@@ -11,7 +12,7 @@ dossier = dossier.replace(os.sep, '/') #transformation
 
 
 liste_res = []
-with open(f'{dossier}/importation_objects/{fichier}','r', newline='') as csvfile:
+with open(f'{dossier}/test_unitaire/{FICHIER}','r', newline='') as csvfile:
     reader = csv.reader(csvfile, delimiter= ',')
     for row in reader:
         liste_res.append(row[0])
