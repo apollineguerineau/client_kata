7eafb06709858d9558370f606b4be6bcb10a341e
Mathieu Thomassin <mathieu.thomassin@eleve.ensai.fr>
2022-11-22 12:09:56 +0100

    pylint creerlistemanuelleview

--- view/creerlistemanuelleview.py
+++ view/creerlistemanuelleview.py
@@ -1,14 +1,10 @@
-from email.message import Message
-from pprint import pprint
-
+"""gère l'affichage pour créer une liste manuellement
+"""
 
 from InquirerPy import inquirer
-from InquirerPy.base.control import Choice
 
 from view.abstractview import AbstractView
 from view.session import Session
-
-
 
 
 ASK_NOM_LISTE=inquirer.text(message = 'Quel est le nom de ta nouvelle liste?')
@@ -16,11 +12,14 @@ ASK_PREMIER_MOT=inquirer.text(message = 'Quel est le premier mot de la liste?')
 
 #pylint: disable=import-outside-toplevel
 #justification: permet d'éviter les imports circulaires (TP du prof et crash test)
+#pylint: disable=no-value-for-parameter
+# justification : voir axe d'amélioration sur les bloc TRY Except + doc API
 class CreerListeManuelleView(AbstractView):
-
+    """permet de créer des listes manuellement en console
+    """
 
     def display_info(self):
-        print(f"Création d'une liste")
+        print("Création d'une liste")
 
     def make_choice(self):
         nom_liste = ASK_NOM_LISTE.execute()
@@ -31,13 +30,16 @@ class CreerListeManuelleView(AbstractView):
         proposition = Proposition(mot)
         mot = proposition.mot
 
-        #On récupère ensuite le nom de toutes les listes du joueur pour ne pas créer deux listes avec le même nom
+        #On récupère ensuite le nom de toutes les listes du
+        # joueur pour ne pas créer deux listes avec le même nom
         id_joueur = (Session().joueur.id_joueur)
         from client_joueur import ClientJoueur
         clientjoueur = ClientJoueur()
+        #pylint: disable=unused-import
+        #justification: get_listes retourne une liste de Liste(s)
         listes = clientjoueur.get_listes(id_joueur)
         from business_objects.liste import Liste
-        if listes != None :
+        if listes is not None :
             for liste in listes :
                 if liste.nom == nom_liste :
                     print("Tu as déjà une liste avec ce nom")
@@ -52,4 +54,3 @@ class CreerListeManuelleView(AbstractView):
 
         from view.accueilpersoview import AccueilPersoView
         return AccueilPersoView()
-
