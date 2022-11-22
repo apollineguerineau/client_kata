e0831e9fefd2e589f17b93b7cf42cbc052b2fc18
Mathieu Thomassin <mathieu.thomassin@eleve.ensai.fr>
2022-11-22 18:04:37 +0100

    pylint modificationlisteview.py

--- view/modificationlisteview.py
+++ view/modificationlisteview.py
@@ -1,16 +1,18 @@
+"""gère l'affichage lors de la modification d'une liste
+"""
 from InquirerPy import inquirer
 from InquirerPy.base.control import Choice
 
 from view.abstractview import AbstractView
-from view.session import Session
 
 #pylint: disable=import-outside-toplevel
 #justification: permet d'éviter les imports circulaires (TP du prof et crash test)
 class ModificationListeView (AbstractView) :
-
+    """gère l'affichage lors de la modification d'une liste
+    """
     def __init__(self):
         self.__questions = inquirer.select(
-            message=f'Que souhaites tu faire maintenant?'
+            message='Que souhaites tu faire maintenant?'
             , choices=[
                 Choice('Ajouter un mot')
                 ,Choice('Supprimer un mot')
@@ -23,14 +25,12 @@ class ModificationListeView (AbstractView) :
 
     def make_choice(self):
         reponse = self.__questions.execute()
-        if reponse == 'Nothing':
-            pass
-        elif reponse== 'Ajouter un mot':
+        if reponse== 'Ajouter un mot':
             from view.ajoutermotview import AjouterMotView
             return AjouterMotView()
-        elif reponse== 'Supprimer un mot':
+        if reponse== 'Supprimer un mot':
             from view.supprimermotview import SupprimerMotView
             return SupprimerMotView()
-        elif reponse== "Retour à l'accueil":
-            from view.accueilpersoview import AccueilPersoView
-            return AccueilPersoView()
+        #elif reponse== "Retour à l'accueil":
+        from view.accueilpersoview import AccueilPersoView
+        return AccueilPersoView()
