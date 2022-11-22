9d9f112509fce4b9259d74927ec009392cd669be
Mathieu Thomassin <mathieu.thomassin@eleve.ensai.fr>
2022-11-22 18:25:00 +0100

    pylint propositionview.py

--- view/propositionview.py
+++ view/propositionview.py
@@ -1,29 +1,37 @@
+"""gère l'affichage pour faire une proposition au cours d'une partie
+"""
+import time
 from InquirerPy import inquirer
-from InquirerPy.base.control import Choice
+
+#pylint: disable=unused-import
+#justification: on utilise des objets à importer quand même. cf amélioration possible
+from business_objects.partie import Partie
+from business_objects.difficultes import Difficultes
 
 from view.abstractview import AbstractView
 from view.session import Session
 
-from business_objects.partie import Partie
-from business_objects.difficultes import Difficultes
-
-import time
 
 #pylint: disable=import-outside-toplevel
 #justification: permet d'éviter les imports circulaires (TP du prof et crash test)
 class PropositionView(AbstractView) :
 
+    """gère l'affichage pour faire une proposition au cours d'une partie
+    """
     def display_info(self):
         pass
 
     def make_choice(self):
+        #pylint: disable=too-many-locals
+        #justification: on a besoin de ces variables locales
         prop_faites = len(Session().partie.liste_mots_proposes)
         nb_tentatives = int(Session().partie.difficultes.nb_tentatives)
 
         nb_prop_restantes = nb_tentatives - prop_faites
         temps = time.time()
-        ASK_PROPOSITION =inquirer.text(message = f'Quel mot veux tu proposer? Il te reste {nb_prop_restantes} proposition(s)')
-        mot = ASK_PROPOSITION.execute()
+        ask_proposition =inquirer.text(
+            message = f'Quel mot veux tu proposer? Il te reste {nb_prop_restantes} proposition(s)')
+        mot = ask_proposition.execute()
         temps2 = time.time()
         if temps2 - temps > float(Session().partie.difficultes.temps) :
             print("Tu as dépassé le temps autorisé pour faire une proposition")
@@ -39,11 +47,13 @@ class PropositionView(AbstractView) :
         Session().partie.liste_mots_proposes.append(proposition.mot)
         prop_faites = len(Session().partie.liste_mots_proposes)
         nb_prop_restantes = nb_tentatives - prop_faites
-        if (proposition.est_autorise() or partie.est_liste_perso) and len(proposition.mot) == partie.difficultes.nb_lettres  :
+        if (proposition.est_autorise() or partie.est_liste_perso) \
+        and len(proposition.mot) == partie.difficultes.nb_lettres  :
             propositionverifiee = partie.verifie_proposition(proposition)
             print(propositionverifiee)
         else :
-            print(f"Le mot proposé n'est pas autorisé. Le mot attendu est de {partie.difficultes.nb_lettres} lettres") 
+            print("Le mot proposé n'est pas autorisé. " +
+                  "Le mot attendu est de {partie.difficultes.nb_lettres} lettres")
 
 
         mot_obj = Proposition(partie.mot_objectif)
@@ -61,18 +71,10 @@ class PropositionView(AbstractView) :
             from view.accueilpersoview import AccueilPersoView
             return AccueilPersoView()
 
-        elif nb_prop_restantes == 0 :
+        if nb_prop_restantes == 0 :
             print("Tu as perdu car tu as dépassé le nombre de tentatives autorisé")
             from view.accueilpersoview import AccueilPersoView
             return AccueilPersoView()
 
-        else :
-            from view.pauseview import PauseView
-            return PauseView()
-
-
-
-
-
-
-
+        from view.pauseview import PauseView
+        return PauseView()
