d237f768c1486106a293e672d653090baa73ffd8
Mathieu Thomassin <mathieu.thomassin@eleve.ensai.fr>
2022-11-22 19:13:05 +0100

    pylint toptenpersoview.py

--- view/toptenpersoview.py
+++ view/toptenpersoview.py
@@ -1,5 +1,5 @@
-from InquirerPy import inquirer
-from InquirerPy.base.control import Choice
+"""gère l'affichage des 10 meilleurs scores du joueur en cours
+"""
 
 from view.abstractview import AbstractView
 from view.session import Session
@@ -8,12 +8,14 @@ from view.session import Session
 #pylint: disable=import-outside-toplevel
 #justification: permet d'éviter les imports circulaires (TP du prof et crash test)
 class TopTenPersoView(AbstractView):
-
-
+    """gère l'affichage des 10 meilleurs scores du joueur en cours
+    """
     def display_info(self):
-        print(f"Voici tes 10 meilleurs scores")
+        print("Voici tes 10 meilleurs scores")
 
     def make_choice(self):
+        #pylint: disable=unused-import
+        #justification: on utilise des objets à importer quand même. cf amélioration possible
         from business_objects.joueur import Joueur
         meilleurs_scores = Session().joueur.topten
         for score in meilleurs_scores :
