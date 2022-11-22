524588c16c47694b216da0953a07f4a0aaa86f49
Mathieu Thomassin <mathieu.thomassin@eleve.ensai.fr>
2022-11-22 19:15:53 +0100

    pylint toptenview.py

--- view/toptenview.py
+++ view/toptenview.py
@@ -1,17 +1,17 @@
-from InquirerPy import inquirer
-from InquirerPy.base.control import Choice
-
+"""gère l'affichage des 10 meilleurs scores globaux
+"""
 from view.abstractview import AbstractView
-from view.session import Session
 
 
 #pylint: disable=import-outside-toplevel
 #justification: permet d'éviter les imports circulaires (TP du prof et crash test)
 class ViewTopTen(AbstractView):
+    """gère l'affichage des 10 meilleurs scores globaux
+    """
 
 
     def display_info(self):
-        print(f"Voici les 10 meilleurs scores")
+        print("Voici les 10 meilleurs scores")
 
     def make_choice(self):
         from client_top_ten_general import ClientTopTen
@@ -21,6 +21,3 @@ class ViewTopTen(AbstractView):
             print(score)
         from view.accueilkataview import AccueilKataView
         return AccueilKataView()
-
-
-
