cdcfdf59094c9eadac2cd2b11f525de5287b6864
Mathieu Thomassin <mathieu.thomassin@eleve.ensai.fr>
2022-11-22 09:35:30 +0100

    pylint accueilpersoview.py

--- view/session.py
+++ view/session.py
@@ -1,8 +1,8 @@
 from utils.singleton import Singleton
-
+from business_objects.joueur import Joueur
 
 class Session(metaclass=Singleton):
-    def __init__(self, joueur, partie = None, liste = None):
+    def __init__(self, joueur: Joueur, partie = None, liste = None):
         """
         Définition des variables que l'on stocke en session
 
@@ -10,11 +10,11 @@ class Session(metaclass=Singleton):
         ----------
 
         joueur : Joueur
-        Joueur connecté
+            Joueur connecté
 
         partie : Partie
-        Partie en cours du joueur connecté
-        Si le joueur n'a pas de partie en cours, partie vaut None
+            Partie en cours du joueur connecté
+            Si le joueur n'a pas de partie en cours, partie vaut None
 
         liste : str
         Nom de la liste en cours d'utilisation
