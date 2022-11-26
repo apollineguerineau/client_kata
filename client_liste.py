fa7e462f666683883d94e331bad816b19ed74551
Mathieu Thomassin <mathieu.thomassin@eleve.ensai.fr>
2022-11-26 16:03:02 +0100

    pylint client_liste.py

--- client_liste.py
+++ client_liste.py
@@ -1,26 +1,37 @@
-import os
-from typing import List, Optional
-from utils.singleton import Singleton
+"""couche service: gère les requetes du client vers l'API pour les listes de mots
+"""
+
 import requests
+
+from utils.singleton import Singleton
+
+
 from business_objects.liste import Liste
 
 
 END_POINT="/liste"
 
 class ClientListe(metaclass= Singleton):
-
+    """gère le endpoint pour les listes de mots
+    """
+    #pylint: disable=unused-variable
+    #justification: les "req =" sont nécessaires pour lancer une requete
     def __init__(self) -> None:
+        #pylint: disable=invalid-name
+        #justification: le __HOST pourrait reservir pour un déploiement
         self.__HOST ="http://127.0.0.1:80"
 
 
-    def get_mot(self, id:int) :
+    def get_mot(self, identifiant_mot:int) :
         '''Retourne un mot selon son identifiant
 
-        Parameters: l'identifiant du mot : int
-
+        Parameters:
+        identifiant_mot: int
+            l'identifiant du mot
         Returns:
-            str : le mot'''
-        req = requests.get(f"{self.__HOST}{END_POINT}/{id}")
+            str : le mot
+        '''
+        req = requests.get(f"{self.__HOST}{END_POINT}/{identifiant_mot}")
         return(req.json())
 
 
@@ -44,4 +55,3 @@ class ClientListe(metaclass= Singleton):
         Parameters: l'identifiant de la liste : int'''
 
         req = requests.delete(f"{self.__HOST}{END_POINT}/{id_liste}")
-
